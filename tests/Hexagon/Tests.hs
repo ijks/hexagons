aa05c451b38218f847446f882201459f7f9180a5
Daan Rijks <daanrijks@gmail.com>
2016-11-18 16:08:45 +0100

    Remove type signatures from propertie definitions

--- tests/Hexagon/Tests.hs
+++ tests/Hexagon/Tests.hs
@@ -30,21 +30,16 @@ tests = testGroup "Hexagon"
 instance (Arbitrary a, Num a) => Arbitrary (Hex a) where
     arbitrary = fromAxial <$> arbitrary
 
-cubeIdentity :: (Eq a, Num a, Show a) => Hex a -> Bool
 cubeIdentity = checkIdentity cubeCoords fromCube
 
-axialIdentity :: (Eq a, Num a, Show a) => Hex a -> Bool
 axialIdentity = checkIdentity axialCoords fromAxial
 
 instance Arbitrary Parity where
     arbitrary = elements [Odd, Even]
 
-offsetIdentity :: (Eq a, Integral a, Show a) => Parity -> Hex a -> Bool
 offsetIdentity par = checkIdentity (offsetCoords par) (fromOffset par)
 
-cubePlanar :: (Eq a, Num a, Show a) => Hex a -> Bool
 cubePlanar = planar . cubeCoords
     where planar (x, y, z) = x + y + z == 0
 
-neighborsCount :: Num a => Hex a -> Bool
 neighborsCount h = length (neighbors h) == 6
