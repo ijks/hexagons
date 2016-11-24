bfd6e1d096572e4e7623f7caeab14902a31617f2
Daan Rijks <daanrijks@gmail.com>
2016-11-24 21:35:35 +0100

    Add a test for pixel coordinate conversions

--- tests/Hexagon/Tests.hs
+++ tests/Hexagon/Tests.hs
@@ -21,6 +21,8 @@ tests = testGroup "Hexagon"
         (axialIdentity :: Hex Int -> Bool)
     , testProperty "conversion to and then from offset is idempotent"
         (offsetIdentity :: Parity -> Hex Int -> Bool)
+    , testProperty "conversion to and then from pixel is idempotent"
+        (pixelIdentity :: Hex Float -> Bool)
     , testProperty "cube coords are always in the same plane"
         (cubePlanar :: Hex Int -> Bool)
     , testProperty "a hexagon has exactly 6 neighbors"
@@ -38,6 +40,8 @@ instance Arbitrary Parity where
     arbitrary = elements [Odd, Even]
 
 offsetIdentity par = checkIdentity (offsetCoords par) (fromOffset par)
+
+pixelIdentity h = approxEq 1e-4 h (fromPixel (pixelCoords h))
 
 cubePlanar = planar . cubeCoords
     where planar (x, y, z) = x + y + z == 0
--- tests/Util.hs
+++ tests/Util.hs
@@ -2,3 +2,6 @@ module Util where
 
 checkIdentity :: (Eq a, Show a) => (a -> b) -> (b -> a) -> a -> Bool
 checkIdentity to from x = from (to x) == x
+
+approxEq :: (Ord a, Num a) => a -> a -> a -> Bool
+approxEq epsilon a b = abs (a - b) < epsilon
