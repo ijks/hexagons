41bb7e9793aef018b41c3197f05fc0c4e95182c7
Daan Rijks <daanrijks@gmail.com>
2017-06-11 18:48:47 +0200

    Refactor 'drawGrid' to be more flexible

--- src/Main.purs
+++ src/Main.purs
@@ -7,11 +7,13 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Foldable (class Foldable, foldMap)
 import Data.Int (toNumber)
-import Data.List ((..))
+import Data.List (List, (..))
 import Data.Maybe (isJust)
+import Data.Monoid (class Monoid, mempty)
+import Data.Tuple (Tuple(..))
 import Graphics.Drawing (Drawing, Shape)
 import Graphics.Drawing (closed) as Shape
-import Graphics.Drawing (scale) as Drawing
+import Graphics.Drawing (scale, translate) as Drawing
 import Graphics.Canvas (CANVAS)
 import Math (cos, pi, sin)
 
@@ -29,14 +31,17 @@ main = runFlareDrawing "controls" "drawing" $ pure drawing
 
 drawing :: Drawing
 drawing = Drawing.scale 10.0 10.0
-  $ drawGrid
+  $ drawGrid drawHex (const mempty)
   $ fromCoordinates (const unit)
   $ square Even { width: 10, height: 10 }
-
-drawGrid :: forall a. Grid a -> Drawing
-drawGrid grid = foldMap drawHex $ coordinates grid
   where
-    drawHex h =
+    drawHex = const $ draw (lineColor black <> lineWidth 0.1) $ hex 0.0 0.0 1.0
+
+drawGrid :: forall a. (Hex Int -> Drawing) -> (a -> Drawing) -> Grid a -> Drawing
+drawGrid drawHex drawContents grid =
+  foldMap drawItem $ (toUnfoldable grid :: List _)
+  where
+    drawItem (Tuple h x) =
       let coords = pixelCoords $ map toNumber h
-      in draw style $ hex coords.x coords.y 1.0
-    style = lineColor black <> lineWidth 0.1
+      in Drawing.translate coords.x coords.y
+        $ drawHex h <> drawContents x
