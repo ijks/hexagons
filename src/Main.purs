88ea935ab5d0288db90922f997f5675e10d29ee1
Daan Rijks <daanrijks@gmail.com>
2017-06-16 01:29:02 +0200

    Add random generation & setting the grid size

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Color (black)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Data.Foldable (class Foldable, foldMap)
 import Data.Int (toNumber)
 import Data.List (List, (..))
@@ -18,31 +19,37 @@ import Graphics.Canvas (CANVAS)
 import Math (cos, pi, sin)
 
 import DOM (DOM)
-import Flare (UI, numberSlider)
+import Flare (UI, intSlider, numberSlider)
 import Flare.Drawing (runFlareDrawing)
 import Signal.Channel (CHANNEL)
 
 import Drawing
-import Hexagon
+import Generate
 import Grid
+import Hexagon
 
 main :: Eff (console :: CONSOLE, dom :: DOM, channel :: CHANNEL, canvas :: CANVAS) Unit
 main = runFlareDrawing "controls" "drawing" $ ui
 
 ui :: forall e. UI e Drawing
 ui = drawing
-  <$> numberSlider "X Offset" (-10.0) 10.0 0.1 0.0
+  <$> intSlider "Width" 0 20 10
+  <*> intSlider "Height" 0 20 10
+  <*> numberSlider "X Offset" (-10.0) 10.0 0.1 0.0
   <*> numberSlider "Y Offset" (-10.0) 10.0 0.1 0.0
   <*> numberSlider "Scale" 1.0 20.0 0.1 1.0
 
-drawing :: Number -> Number -> Number -> Drawing
-drawing x y s = Drawing.scale s s
+drawing :: Int -> Int -> Number -> Number -> Number -> Drawing
+drawing w h x y s = Drawing.scale s s
   $ Drawing.translate x y
-  $ drawGrid drawHex (const mempty)
-  $ fromCoordinates (const unit)
-  $ square Even { width: 10, height: 10 }
+  $ drawGrid drawHex drawBool
+  -- FIXME: Don't do this!!!
+  $ unsafePerformEff $ runRandom $ noise shape
   where
     drawHex = const $ draw (lineColor black <> lineWidth 0.1) $ hex 0.0 0.0 1.0
+    drawBool = if _ then draw (fillColor black) $ hex 0.0 0.0 0.5 else mempty
+    shape = fromCoordinates (const unit) $ square Even { width: w, height: h }
+
 
 drawGrid :: forall a. (Hex Int -> Drawing) -> (a -> Drawing) -> Grid a -> Drawing
 drawGrid drawHex drawContents grid =
